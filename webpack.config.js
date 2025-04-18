f7dc4dfb898d8ee6d2e9e06c7235f03da4b26138
Funny Ivri <itamarivri.best@gmail.com>
2025-04-18 18:35:36 +0300

    asdasdasdasd

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.js', // or your JS file
     output: {
       filename: 'main.js',
       path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        // no need for a special rule for JSON
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.json'], // optional, .json is included by default
     },
   mode: 'production'
 };
